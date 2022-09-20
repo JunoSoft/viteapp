baaced950fb668768533904c8aa931ab63f61af5
Junosoft <junosoftltd@gmail.com>
2022-09-20 21:31:07 +0200

    edit

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function index(props) {
+  onchangeHandler = (event)=>{
+event.preventDefault();
+const a = 4;
+return a
+  }
   return (
     <div>
       GEO lOCATION DATA
+      <input type="text" onChange={onchangeHandler}/>◘
     </div>
   );
 }

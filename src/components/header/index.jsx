1cf42e30d7b86db08f7a6c76e54cb287eb5bb544
Junosoft <junosoftltd@gmail.com>
2022-09-22 23:40:20 +0200

    edition

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,8 @@ function index(props) {
   return (
     <div>
       <h2>Main Tittle</h2>
-      <h3>SUb title</h3>
+      <h3>SUb title</h3><h1>dtydf</h1>
+      <
       <div>Hope everyone is going alright</div>
     </div>
   );

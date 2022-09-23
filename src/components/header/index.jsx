11a5b761509481446d5914836dc1d32f69127beb
Junosoft <junosoftltd@gmail.com>
2022-09-23 23:33:04 +0200

    paged edit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,6 +5,7 @@ function index(props) {
     <div>
       <h2>Main Tittle</h2>
       <h3>SUb title</h3><h1>dtydf</h1>
+      <h2>dfhgdf</h2>
       
       <div>Hope everyone is going alright</div>
     </div>

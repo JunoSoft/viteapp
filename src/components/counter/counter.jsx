8bc59232ebc59efd0f6daa993aca52a45c8197ca
Junosoft <junosoftltd@gmail.com>
2022-09-25 23:01:45 +0200

    editing xxx

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -5,7 +5,7 @@ function counter(props) {
     <div>
       Counter :45
       Numbers:89
-      hello my people s
+      
     </div>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,9 +4,7 @@ function index(props) {
   return (
     <div>
       <h2>Main Tittle</h2>
-      <h3>SUb title</h3><h1>dtydf</h1>
-      <h2>dfhgdf</h2>
-      <div>gsfgsd</div>
+      
       
       <div>Hope everyone is going alright a</div>
     </div>
